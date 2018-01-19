3d631e197916b7cd251e7cd71ba867955fe08825
Nicolas-Gras <nicolas.gras@desjardins.com>
2018-01-19 13:52:27 -0500

    React.Compoment concept

--- tic-tac-toe-app/src/index.js
+++ tic-tac-toe-app/src/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class MyClass extends React.Component {
+    render() {
+        return (
+            
+            <div>
+                <p>By default, this.props object is empty</p>
+                <p>props name: {this.props.name}</p>
+            </div>
+            
+        );
+    }
+}
+
+ReactDOM.render(
+    <MyClass />,
+    document.getElementById('root')
+);
